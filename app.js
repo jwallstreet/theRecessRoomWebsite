a4467398826eb03827d95a03dd6a6f14d39c7afa
Andy Nguyen <andynnguyen@gmail.com>
2016-05-11 08:45:33 -0700

    created a static file server

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict';
+var express = require('express');
+var path = require('path');
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
+    var addr = server.address();
+    console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
+
+
+app.use(express.static('client/app'));
+
